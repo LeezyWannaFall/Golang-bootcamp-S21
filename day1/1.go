dbb0f538dbc41fdd36035bbe2b0208357bdcfef8
LeezyWannaFall <191919qaz@gmail.com>
2025-10-16 00:15:37 +0300

    1st quest complete

---
+++ day1/1.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x, y float64
+	var operation string
+	scan(&x, &y, &operation)
+	calculate(x, y, operation)
+}
+
+func scan(x, y *float64, operation *string) {
+	for {
+		fmt.Print("Input right operand: ")
+		if _, err := fmt.Scan(x); err == nil {
+			break
+		}
+		fmt.Print("Invalid input. Please enter a number.\n")
+	}
+
+	ValidOperators := []string{"+", "-", "*", "/"}
+	for {
+		fmt.Print("Input operation (+, -, *, /): ")
+		fmt.Scan(operation)
+
+		isValid := false
+		for _, v := range ValidOperators {
+			if *operation == v {
+				isValid = true
+				break
+			}
+		}
+
+		if isValid {
+			break
+		}
+
+		fmt.Print("Invalid operation. Please enter valid operations (+, -, *, /).\n")
+	}
+
+	for {
+		fmt.Print("Input left operand: ")
+		if _, err := fmt.Scan(y); err == nil {
+			break
+		}
+		fmt.Print("Invalid input. Please enter a number.\n")
+	}
+}
+
+func calculate(x, y float64, operation string) {
+	switch (operation) {
+	case "+":
+		fmt.Printf("Result: %.3f", x + y)
+	case "-":
+		fmt.Printf("Result: %.3f", x - y)
+	case "*":
+		fmt.Printf("Result: %.3f", x * y)
+	case "/":
+		if y == 0 {
+			fmt.Print("Error: division by zero")
+		} else {
+			fmt.Printf("Result: %.3f", x / y)
+		}
+	}
+}
+
