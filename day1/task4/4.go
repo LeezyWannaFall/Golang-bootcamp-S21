1be821da37f8bf12b88353666adfb7b891a82366
LeezyWannaFall <191919qaz@gmail.com>
2025-11-02 00:39:36 +0300

    done 4

--- day1/task4/4.go
+++ day1/task4/4.go
@@ -16,6 +16,14 @@ func main() {
 	RunCommand()
 }
 
+type PatientNotFoundError struct {
+	Message string
+}
+
+func (e *PatientNotFoundError) Error() string {
+	return e.Message
+}
+
 func RunCommand() {
 	m := make(map[string][]PatientInfo)
 
@@ -34,36 +42,25 @@ func RunCommand() {
 			m[name] = append(m[name], PatientInfo{doctor: doctor, date: date})
 		case "GetHistory", "gethistory":
 			name := ReadName()
-			
-			visits, ok := m[name]
-			if !ok {
-				fmt.Println("patient not found")
-			} else {
-				for _, v := range visits {
-					fmt.Print("Doctor: ", v.doctor, "\n", "Date: ", v.date, "\n")
-				}
+			err := GetHistory(m, name)
+
+			if err != nil {
+				fmt.Print(err)
 			}
 		case "GetLastVisit", "getlastvisit":
 			var doctor string
 			name := ReadName()
 			fmt.Scanln(&doctor)
 			ValidDoctor := true
-			visits, ok := m[name]
-			if !ok {
-				fmt.Print("patient not found\n")
-			} else {
-				for _, v := range visits {
-					if doctor == v.doctor {
-						fmt.Println("Date: ", v.date)
-						ValidDoctor = true
-					} else {
-						ValidDoctor = false
-					}
-				}
-			}
 
-			if !ValidDoctor {
-				fmt.Println("invalid doctor")
+			err, ValidDoctor := GetLastVisit(m, name, doctor, ValidDoctor)
+
+			if err != nil {
+				fmt.Print(err)
+			} else {
+				if !ValidDoctor {
+					fmt.Println("invalid doctor")
+				}
 			}
 		case "Exit", "exit":
 			return
@@ -78,4 +75,33 @@ func ReadName() string {
 	line,_ := reader.ReadString('\n')
 	name := strings.TrimSpace(line)
 	return name
+}
+
+func GetHistory(m map[string][]PatientInfo, name string) error {
+	visits, ok := m[name]
+	if !ok {
+		return &PatientNotFoundError{Message: "patient not found\n"}
+	} else {
+		for _, v := range visits {
+			fmt.Print("Doctor: ", v.doctor, " Date: ", v.date, "\n")
+		}
+	}
+	return nil
+}
+
+func GetLastVisit(m map[string][]PatientInfo, name, doctor string, ValidDoctor bool) (error, bool) {
+	visits, ok := m[name]
+	if !ok {
+		return &PatientNotFoundError{Message: "patient not found\n"}, false
+	} else {
+		for _, v := range visits {
+			if doctor == v.doctor {
+				fmt.Println("Date: ", v.date)
+				ValidDoctor = true
+			} else {
+				ValidDoctor = false
+			}
+		}
+	}
+	return nil, ValidDoctor
 }
