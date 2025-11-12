50667b705fdc90715f3095d6ba5664fc33af1eb1
Leezy <191919qaz@gmail.com>
2025-11-12 21:02:30 +0300

    task3 done

--- day2/task3/3.go
+++ day2/task3/3.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -15,9 +18,30 @@ func main() {
 	K, err := strconv.ParseUint(os.Args[1], 10, 64)
 
 	if err != nil {
-		fmt.Print("Error: Argument must have type int")
+		fmt.Print("Error: Argument must have type uint")
 		return
 	}
 
+	TickCounter := 0
+	TimeCounter := K
+	SignalChannel := make(chan os.Signal, 1)
+	signal.Notify(SignalChannel, syscall.SIGINT, syscall.SIGTERM)
 
+	go Ticker(TickCounter, TimeCounter, K)
+
+	SigRes, ok := <-SignalChannel
+	if ok {
+		fmt.Println("Got signal:", SigRes)
+		fmt.Println("Termination")
+		return
+	}
+}
+
+func Ticker(TickCounter int, TimeCounter, K uint64) {
+	for {
+		time.Sleep(time.Duration(K) * time.Second)
+		TickCounter++
+		fmt.Println("Tick", TickCounter, "since", TimeCounter)
+		TimeCounter += K
+	}
 }
