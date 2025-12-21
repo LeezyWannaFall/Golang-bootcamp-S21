8ab0dce7fa7f7ad3610a561338a3b0d9dd56af65
LeezyWannaFall <191919qaz@gmail.com>
2025-12-21 08:14:28 +0300

    fix random

--- roguelike/domain/logic/random.go
+++ roguelike/domain/logic/random.go
@@ -1,12 +1,15 @@
 package logic
 
-import 	"math/rand/v2"
+import (
+	"math/rand/v2"
+	"roguelike/domain/datastructs"
+)
 
 func GetRandomInRange(min, max int) int {
 	return rand.IntN(max-min+1) + min
 }
 
-func ShuffleEdges(edges []Edge) {
+func ShuffleEdges(edges []datastructs.Edge) {
 	rand.Shuffle(len(edges), func(i, j int) {
 		edges[i], edges[j] = edges[j], edges[i]
 	})
