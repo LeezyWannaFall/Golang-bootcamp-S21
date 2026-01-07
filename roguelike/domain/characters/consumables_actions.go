8d48e20bf0745b333210c7509582fc9ff50ee252
LeezyWannaFall <191919qaz@gmail.com>
2026-01-07 04:21:45 +0300

    todo consumables

--- roguelike/domain/characters/consumables_actions.go
+++ roguelike/domain/characters/consumables_actions.go
@@ -4,3 +4,72 @@ import (
 	"roguelike/domain/entity"
 )
 
+type ConsumableType int
+
+const ( 
+	NoneType ConsumableType = iota
+	FoodType
+	ElixirType
+	WeaponType
+	ScrollType
+)
+
+func EqualWeapons(weapon1, weapon2 entity.Weapon) bool {
+	return weapon1.Strength == weapon2.Strength && weapon1.Name == weapon2.Name
+}
+
+func CheckConsumables(player *entity.Player, room entity.Room) {
+	WasConsumed := false
+}
+
+func TakeScroll(backpack entity.Backpack, scroll entity.Scroll, room entity.Room) {
+
+}
+
+func TakeElixir(backpack entity.Backpack, elixir entity.Elixir, room entity.Room) {
+
+}
+
+func TakeFood(backpack entity.Backpack, food entity.Food, room entity.Room) {
+
+}
+
+func TakeWeapon(backpack entity.Backpack, weapon entity.Weapon, room entity.Room) {
+
+}
+
+func DeleteFromRoom(room entity.Room, consumableCoords entity.Object, consumableType entity.ConsumableType) {
+
+}
+
+func ThrowOnGround(player entity.Player, room *entity.Room, weapon entity.Weapon) {
+
+}
+
+func CheckUnoccupiedLevel(level *entity.Level, coords entity.Object) bool {
+
+}
+
+func UseConsumable(player *entity.Player, consumablesType ConsumableType, room *entity.Room, index int) {
+
+}
+
+func EatFood(player *entity.Player, food entity.Food) {
+
+}
+
+func DrinkElixir(player *entity.Player, elixir entity.Elixir) {
+
+}
+
+func ReadScroll(player *entity.Player, scroll entity.Scroll) {
+
+}
+
+func RemoveFromBackpack(backpack entity.Backpack, consumableType ConsumableType, index int) {
+
+}
+
+func CheckTempEffectEnd(player *entity.Player) {
+
+}
