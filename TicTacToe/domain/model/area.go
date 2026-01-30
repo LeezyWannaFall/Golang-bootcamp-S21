0dec5d14a2f0e363c21538925163c3d2bd842c81
LeezyWannaFall <191919qaz@gmail.com>
2026-01-31 02:43:39 +0300

    area.go edit

---
+++ TicTacToe/domain/model/area.go
@@ -0,0 +1,39 @@
+package domain
+
+const FieldSize = 3
+
+type GameField struct {
+	Cells [][]int
+}
+
+func GenerateField() *GameField {
+	field := make([][]int, FieldSize)
+	for i := 0; i < FieldSize; i++ {
+		field[i] = make([]int, FieldSize)
+	}
+	return &GameField{
+		Cells: field,
+	}
+}
+
+func (f* GameField) ClearField() {
+	for i := 0; i < FieldSize; i++ {
+		for j := 0; j < FieldSize; j++ {
+			f.Cells[i][j] = 0
+		}
+	}
+}
+
+func (f* GameField) PlaceSymbolOnField(x, y int, symbol int) bool {
+	if x < 0 || x >= FieldSize || y < 0 || y >= FieldSize {
+		return false
+	}
+
+	if f.Cells[y][x] != 0 {
+		return false
+	}
+
+	f.Cells[y][x] = symbol
+	return true
+}
+
