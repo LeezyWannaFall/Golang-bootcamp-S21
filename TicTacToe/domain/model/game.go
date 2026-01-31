c66909d32443d60cc424e34514ba1c4d56702d7f
LeezyWannaFall <191919qaz@gmail.com>
2026-02-01 00:10:17 +0300

    game.go

---
+++ TicTacToe/domain/model/game.go
@@ -0,0 +1,96 @@
+package domain
+
+const (
+	Empty = 0
+	Cross = 1
+	Zero = 2
+)
+
+type Game struct {
+	IsFinished bool
+	Field GameField
+	CurrentTurn int
+	Winner int
+}
+
+func NewGameState() *Game {
+	return &Game{
+		IsFinished:  false,
+		CurrentTurn: Cross,
+		Winner:      0,
+	}
+}
+
+func (g *Game) EndGame() {
+	g.IsFinished = true
+}
+
+func (g *Game) IsGameRunning() bool {
+	return g.IsFinished
+}
+
+func (g *Game) SwitchTurn() {
+	if g.CurrentTurn == Cross {
+		g.CurrentTurn = Zero
+	} else {
+		g.CurrentTurn = Cross
+	}
+}
+
+func (g *Game) SetWinner(winner int) {
+	g.Winner = winner
+}
+
+func (g* Game) MakeMove(x, y int) bool {
+	if g.IsFinished {
+		return false
+	}
+
+	if !g.Field.PlaceSymbolOnField(x, y, g.CurrentTurn) {
+		return false
+	}
+
+	if g.CheckWin(g.CurrentTurn) {
+		g.Winner = g.CurrentTurn
+		g.IsFinished = true
+		return false
+	}
+
+	g.SwitchTurn()
+	return true
+}
+
+func (g *Game) CheckWin(symbol int) bool {
+	// строки
+	for i := 0; i < FieldSize; i++ {
+		if g.Field.Cells[i][0] == symbol &&
+			g.Field.Cells[i][1] == symbol &&
+			g.Field.Cells[i][2] == symbol {
+			return true
+		}
+	}
+
+	// колонки
+	for j := 0; j < FieldSize; j++ {
+		if g.Field.Cells[0][j] == symbol &&
+			g.Field.Cells[1][j] == symbol &&
+			g.Field.Cells[2][j] == symbol {
+			return true
+		}
+	}
+
+	// диагонали
+	if g.Field.Cells[0][0] == symbol &&
+		g.Field.Cells[1][1] == symbol &&
+		g.Field.Cells[2][2] == symbol {
+		return true
+	}
+
+	if g.Field.Cells[0][2] == symbol &&
+		g.Field.Cells[1][1] == symbol &&
+		g.Field.Cells[2][0] == symbol {
+		return true
+	}
+
+	return false
+}
