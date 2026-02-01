e42da38def7819031fd63e6658517ae652977f37
LeezyWannaFall <191919qaz@gmail.com>
2026-02-01 04:15:59 +0300

    todo minimax

--- TicTacToe/domain/model/area.go
+++ TicTacToe/domain/model/area.go
@@ -1,4 +1,4 @@
-package domain
+package model
 
 const FieldSize = 3
 
---
+++ TicTacToe/domain/service/minimax.go
@@ -0,0 +1,9 @@
+package service
+
+import "TicTacToe/domain/model"
+
+func MiniMax(g model.Game) {
+	if g.IsFinished {
+		return 
+	}
+}
